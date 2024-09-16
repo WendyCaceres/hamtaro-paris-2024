17856245f1f0eb9505e0bc13fe16060477d05a71
TDVCool123 <luisandypp@gmail.com>
2024-09-16 01:08:24 -0400

    merged

--- hamtaro/scripts/main/entrada.gd
+++ hamtaro/scripts/main/entrada.gd
@@ -11,6 +11,6 @@ func _on_body_entered(body: Node2D) -> void:
 		print("El cuerpo es un jugador")
 		transicion.jugador_spam_posicion = Vector2(0, 0)  # Ajusta la posición si es necesario
 		print("Cambiando escena a: res://scene/transicion.tscn")
-		get_tree().change_scene_to_file("res://scene/transicion.tscn")
+		get_tree().change_scene_to_file("res://scene/maps/archery/archery_map.tscn")
 	else:
 		print("El cuerpo no es un jugador")
