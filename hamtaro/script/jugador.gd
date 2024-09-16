9e182467fb93755258d3750edb61692d48a32d9e
WendyCaceres <wcaceres683@gmail.com>
2024-09-16 07:38:26 -0400

    aumento

--- hamtaro/script/jugador.gd
+++ hamtaro/script/jugador.gd
@@ -3,15 +3,12 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 func _ready() -> void:
-	# Asegúrate de que el jugador esté en el grupo "jugador"
 	add_to_group("jugador")
 
 func _physics_process(delta):
-	# Obtener la dirección del input del jugador
 	var dir: Vector2 = Input.get_vector("izquierda", "derecha", "arriba", "abajo")
 	
 	if dir:
-		# Cambiar la animación según la dirección en la que se mueve el personaje
 		if dir.x > 0:
 			$jugador.play("costado_izquierdo")
 		elif dir.x < 0:
@@ -21,11 +18,7 @@ func _physics_process(delta):
 		elif dir.y < 0:
 			$jugador.play("espalda")
 	else:
-		# Detener la animación cuando no hay movimiento
 		$jugador.stop()
 
-	# Establecer la velocidad del personaje
 	velocity = dir * SPEED
-
-	# Mover al personaje con la función `move_and_slide()`
 	move_and_slide()
